a123ab8080e03b32692ab0976951902d6b3a742b
Jhonathan <vianajobs@gmail.com>
2021-12-23 18:27:04 -0300

    Splitting Code Across Multiple Lines

--- Understading JavaScript Basics/script.js
+++ Understading JavaScript Basics/script.js
@@ -2,6 +2,11 @@ let age = 32;
 let userName = 'Max';
 let hobbies = ['Sports', 'Cooking', 'Reading'];
 // index number beginning to 0, 1, 2...
-let job = { title: 'Developer', place: 'Brazil', salary: 50000}
+let job = { 
+    title: 'Developer', 
+    place: 'Brazil', 
+    salary: 50000
+};
 // grouping related data
+
 alert(hobbies[0]);

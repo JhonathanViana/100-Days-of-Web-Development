c47c9ba5862b4961f5922a06134f07472245f13b
Jhonathan <vianajobs@gmail.com>
2021-12-28 15:28:03 -0300

    String Text Operator

--- Understading JavaScript Basics/operations.js
+++ Understading JavaScript Basics/operations.js
@@ -1,24 +1,3 @@
 // math operation
 
-console.log(10 + 4);
-console.log(10 - 4);
-console.log(10 * 4);
-console.log(10 / 4);
-console.log(10 % 4); // 10 / 4 => 2 * 4 => Remainder: 2
-console.log(10 % 3); // 10 / 3 => 3 * 3 => Remainder: 1
-
-console.log((10 + 3 - 5) * 10);
-
-let result = (10 + 3 - 5) * 10;
-result = 10 * 4 ;
-result = result + 1;
-
-result++; // result = result + 1
-result--; // result = result - 1
-
-result += 5; // result = result + 5
-result -= 5; // result = result - 5
-result /= 5; // result = result / 5
-result *= 5; // result = result * 5
-
-console.log(result);
+console.log('Jhonathan' + ' ' + 'Viana');

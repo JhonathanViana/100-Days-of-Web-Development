d8efb01019efb77beaee311c8862d302ce09eba8
Jhonathan <vianajobs@gmail.com>
2021-12-28 15:42:10 -0300

    counting elements

--- Understading JavaScript Basics/operations.js
+++ Understading JavaScript Basics/operations.js
@@ -5,3 +5,7 @@ console.log('Jhonathan' + ' ' + 'Viana');
 let userName = 'Jhonathan';
 console.log(userName.length);
 console.log(userName.toUpperCase());
+
+
+let hobbies = ['Sports', 'Cooking'];
+console.log(hobbies.length); //how many items in this array, in this case, 2
